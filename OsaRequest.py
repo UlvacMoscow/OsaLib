dd1c4d77d7ad5f24eb20b951d7fa39c1989353cf
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2018-04-12 22:22:19 +0300

    add def open_text_to_list, def country_to_currency, def seconds_since_last_change

--- OsaRequest.py
+++ OsaRequest.py
@@ -8,6 +8,7 @@ import osa
 
 URL = 'http://fx.currencysystem.com/webservices/CurrencyServer4.asmx?WSDL'
 CURRENCY = 'currencies.txt'
+COUNTRIES = 'Countries.txt'
 
 
 def server_currency_converter(currency, amount, way, url):
@@ -23,53 +24,30 @@ def server_request(text, url):
             server_currency_converter(str(part[2]), float(part[1]), str(part[0]), url)
 
 
+def open_text_to_list(text, url):
+    with open(text, encoding='utf-8') as data:
+        for line in data:
+            part = line.split()
+            country_to_currency(part[0], url)
+
+
+def country_to_currency(part, url):
+    client = osa.client.Client(url)
+    response = client.service.CountryToCurrency(country=part, activeOnly=True)
+    print('Страна {} код валюты {}'.format(part, response))
+
+
+def seconds_since_last_change(url):
+    client = osa.client.Client(url)
+    response = client.service.SecondsSinceLastChange()
+    print('Последнего обновления курса валют было {} секунд назад'.format(response))
+
+
+seconds_since_last_change(URL)
 server_request(CURRENCY, URL)
-
-# licenseKey=string&
-# fromCurrency=RUB
-# toCurrency=USD
-# amount=100
-# rounding=True
-# format=string&
-# date=string&
-# type=string HTTP/1.1
-#
-#
-# FromUnit = 'degreeFahrenheit'
-# ToUnit = 'degreeCelsius'
-# Temperature = '67'
-
-# def convert(a,b,c):
-#     return client.services(Temperature = a, FromUnit = b, ToUnit = c )
-
-
-# def convert(a,b,c):
-#     params = {
-#         'Temperature' : a,
-#         'FromUnit' : b,
-#         'ToUnit' : c
-#     }
-#     return client.services(params)
-
-# print(convert(Temperature, FromUnit, ToUnit))
+open_text_to_list(COUNTRIES, URL)
 
 
 
-# current_dir = os.path.dirname(os.path.abspath(__file__))
-# temps_dir = os.path.join(current_dir,'temps.txt')
-# currencies_dir = os.path.join(current_dir, 'currencies.txt')
-# travel_dir = os.path.join(current_dir, 'travel.txt')
 
-
-# def read_text(text):
-#     with open(text, encoding='UTF-8') as f:
-#         data = f.read()
-#         text_for_osa = data.split()
-#         client._services.Temperature(text_for_osa[0])
-#         print(text_for_osa[0], 'lasdlsd', text_for_osa[1])
-#
-#
-# print(temps_dir, read_text(temps_dir) )
-# print(currencies_dir, read_text(currencies_dir))
-# print(travel_dir, read_text(travel_dir))
 
